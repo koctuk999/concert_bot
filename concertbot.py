526d9331adcf61993ee197edbbf30648e1fa25f8
koctuk999 <koctuk999@gmail.com>
2018-03-14 16:52:38 +0300

    added menu in pages_keyboard and refactoring

--- concertbot.py
+++ concertbot.py
@@ -2,23 +2,19 @@
 import telebot
 from telebot import types
 from concert_spb import *
-
-token=''
-bot=telebot.TeleBot(token)
-keyboard = types.InlineKeyboardMarkup()
+#Инициализация бота
+token   = ''
+bot     = telebot.TeleBot(token)
+#Клавиатура главного меню и текст
+keyboard= types.InlineKeyboardMarkup()
 keyboard.row(types.InlineKeyboardButton(text='A2',callback_data='A2'))
 keyboard.row(types.InlineKeyboardButton(text='Космонавт',callback_data='kosmo'))
 keyboard.row(types.InlineKeyboardButton(text='Аврора',callback_data='aurora'))
 keyboard.row(types.InlineKeyboardButton(text='Зал ожидания',callback_data='zal'))
-
-@bot.message_handler(commands=['start'])
-def command_start(m):
-    global billboard
-    billboard=[]
-    text='\tПриветствую тебя, мой концертный задрот!\n' \
+text_menu='\tПриветствую тебя, мой концертный задрот!\n' \
          ' Я покажу тебе афишу ближайших концертов СПБ\nВыбери клуб:'
-    bot.send_message(m.chat.id,text=text,reply_markup=keyboard)
 
+#Клавиатура для листания
 def pages_keyboard(start, stop,billboard):
     pag_key = types.InlineKeyboardMarkup()
     btns = []
@@ -27,7 +23,14 @@ def pages_keyboard(start, stop,billboard):
     if stop < len(billboard): btns.append(types.InlineKeyboardButton(
         text='➡', callback_data='to_{}'.format(stop)))
     pag_key.add(*btns)
+    pag_key.add(types.InlineKeyboardButton(text='Меню',callback_data='menu'))
     return pag_key
+
+@bot.message_handler(commands=['start'])
+def command_start(m):
+    global billboard
+    billboard=[]
+    bot.send_message(chat_id=m.chat.id,text=text_menu,reply_markup=keyboard)
 
 def send_billboard(club,m):
     global billboard
@@ -47,17 +50,28 @@ def inline(c):
     elif c.data == 'kosmo':
          send_billboard(c.data, c.message)
     elif c.data == 'A2':
-        pass
+        bot.edit_message_text(chat_id=c.message.chat.id,
+                              message_id=c.message.message_id,
+                              text='К сожалению афиша клуба "А2" пока не доступна(',
+                              reply_markup=keyboard)
     elif c.data =='zal':
-        pass
-    else: #для пролистывания афишы
-        text=''
-        for artist in billboard[int(c.data[3:]):int(c.data[3:])+4]:
-            text+=str(artist)+'\n'
+        bot.edit_message_text(chat_id=c.message.chat.id,
+                              message_id=c.message.message_id,
+                              text='К сожалению афиша клуба "Зал ожидания" пока не доступна(',
+                              reply_markup=keyboard)
+    elif c.data=='menu':
+        bot.edit_message_text(chat_id=c.message.chat.id,
+                              message_id=c.message.message_id,
+                              text=text_menu,
+                              reply_markup=keyboard)
+    else:
+        text = ''
+        for artist in billboard[int(c.data[3:]):int(c.data[3:]) + 4]:
+            text += str(artist) + '\n'
         bot.edit_message_text(chat_id=c.message.chat.id,
                               message_id=c.message.message_id,
                               text=text,
-                              reply_markup=pages_keyboard(int(c.data[3:]),int(c.data[3:])+4,billboard))
+                              reply_markup=pages_keyboard(int(c.data[3:]), int(c.data[3:]) + 4, billboard))
 
 
 def main():
@@ -66,6 +80,6 @@ def main():
     write_in_file('kosmo.txt', pars_kosmonavt())
     print('афиши добавлены в базу')
     print("concert-bot запущен")
-    bot.polling(none_stop=True)
+    bot.polling()
 
 main()
